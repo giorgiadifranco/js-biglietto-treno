8ff1c31bcab9b0866825961725988c62c2ca4f77
giorgiadifranco <giorgiadf08@gmail.com>
2024-09-30 16:39:43 +0200

    fix fase 4

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@
 
 const priceKilometers = 0.21;
 let clientAge;
-let clientOfftwenty = 18;
-let clientOffforty = 65;
 let kilometers;
+
+//let clientOfftwenty = 18;
+//let clientOffforty = 65;
 
 
 
@@ -19,37 +20,42 @@ console.log(clientAge);
 console.log(kilometers);
 
 //calcolo del ticket
-
+//default
 let ticket = priceKilometers*kilometers;
 
+//-20%
 let offTwenty = priceKilometers/100*20;
 let offTwentyticket = offTwenty*kilometers;
 
+//-40%
 let offForty = priceKilometers/100*40;
 let offFortyticket = offForty*kilometers;
+let message = "il prezzo totale del biglietto è €";
 
 //4. elaborazione codice 
 
-if(clientAge>18 && clientAge<65){
+if(clientAge){
       
     ticket = priceKilometers*kilometers;
-   
+    message = "il prezzo totale del biglietto è €"
+   console.log(`${message}${ticket}`)
 
-}console.log(ticket);
-
-else if(clientAge<=18){
+}else if(clientAge<=18){
     
     offTwentyticket = offTwenty*kilometers;
-    
-} console.log(offTwentyticket);
-
-else if(clientAge>=65){
+    message = "il prezzo totale del biglietto è €"
+    console.log(`${message}${offTwentyticket}`);
+}else if(clientAge>=65){
 
     offFortyticket = offForty*kilometers;
-
+    message = "il prezzo totale del biglietto è ";
     
+    console.log(`${message}${offFortyticket}`);
+}
 
-}console.log(offFortyticket);
+
+
+
     
 
 
