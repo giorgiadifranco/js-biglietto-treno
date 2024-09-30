7e01a3dd779762cbcc83c95068a8d233fd829af9
giorgiadifranco <giorgiadf08@gmail.com>
2024-09-30 15:14:45 +0200

    fix conversione da stringa a numero

--- js/main.js
+++ js/main.js
@@ -4,43 +4,24 @@
 
 const priceKilometers = 0.21;
 let clientAge;
-let kilomters;
-let ticket = clientAge*kilomters;
-const offTwenty = 20;
-const offForty = 40;
+let clientOfftwenty = 18;
+let clientOffforty = 65;
+let kilometers;
+//let ticket = priceKilometers*kilometers;
 
-//2.raccolta dati
 
-clientAge = prompt("Età");
+clientAge = prompt("Età"); //il dato in input sarà una stringa da convertire in un valore
+clientAge = parseFloat(clientAge);
 kilometers = prompt("Quanti chilometri devi percorrere?");
+kilometers =parseFloat(kilometers)
 
 console.log(clientAge);
 console.log(kilometers);
 
-//calcolo percentuali
-function percentage{
-    return (priceKilometers/100)*offTwenty;
-}
-console.log(percentage);
+if(clientAge>clientOfftwenty ){
+      
+    let ticket = priceKilometers*kilometers;
+   
 
-
-
-//3. elaborazione del codice
-
-if(age<=18){
-    
-    function percentage{
-        return (priceKilometers/100)*offTwenty;
-    }
-    console.log(percentage)
-}else if(age>=65){
-    function percentage{
-        return (priceKilometers/100)*offForty;
-    }
-    console.log(percentage)
-}
-
-
-
-
+}console.log(ticket);
 
