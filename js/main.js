514902cc962fef6625819c5574ae05f1b2eda84c
giorgiadifranco <giorgiadf08@gmail.com>
2024-09-30 14:17:02 +0200

    add variabili raccolta dati

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//console.log("ciao");
+
+//1. individuzione e creazione delle variabili
+
+const priceKilometers = 0.21;
+let clientAge;
+let kilomters;
+
+//2.raccolta dati
+
+clientAge = prompt("Età");
+kilometers = prompt("Quanti chilometri devi percorrere?");
+
+console.log(clientAge);
+console.log(kilometers);
+
+
+//3. elaborazione del codice
+
+if(age==)
+
+
+
+
+
